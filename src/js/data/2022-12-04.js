4ac42781bb62e18de0fd759478327b29486dd034
HiLord Reilo <dreamerlightsun@outlook.com>
2022-12-04 05:26:31 +0300

    Upload work that have been alreay done
    I started making this fork before actually forking charasort. Intead I just downloaded the source code and messed around with it.

---
+++ src/js/data/2022-12-04.js
@@ -0,0 +1,1022 @@
+dataSetVersion = "2022-12-04"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Remove alternative variations",
+    key: "alt",
+    tooltip: "Check this to remove alternative variations of the same character (ex. Hina (Swimsuit)).",
+	checked: false
+  },
+  {
+    name: "Leave only global version characters",
+    key: "jp",
+    tooltip: "Check this to remove characters that are currently only on japanese server.",
+	checked: false
+  },
+  {
+    name: "Disable NPCs",
+    key: "npc",
+    tooltip: "Check this to remove non-playable (or rather non-usable) characters (ex. Arona).",
+	checked: false
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Aru",
+    img: "d/db/Aru.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Eimi",
+    img: "e/ed/Eimi.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Haruna",
+    img: "a/a6/Haruna.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Hibiki",
+    img: "b/bc/Hibiki.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Hifumi",
+    img: "3/3b/Hifumi.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Hina",
+    img: "8/83/Hina.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Hoshino",
+    img: "a/a9/Hoshino.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Iori",
+    img: "2/26/Iori.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Izumi",
+    img: "1/10/Izumi.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Karin",
+    img: "5/50/Karin.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Maki",
+    img: "2/21/Maki.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Neru",
+    img: "2/24/Neru.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Saya",
+    img: "c/ce/Saya.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Shiroko",
+    img: "6/63/Shiroko.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Shun",
+    img: "1/17/Shun.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Sumire",
+    img: "d/d6/Sumire.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Tsurugi",
+    img: "c/c1/Tsurugi.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Airi",
+    img: "9/96/Airi.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Akane",
+    img: "a/aa/Akane.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Akari",
+    img: "7/7d/Akari.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Ayane",
+    img: "a/a7/Ayane.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Chise",
+    img: "d/d0/Chise.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Fuuka",
+    img: "1/1f/Fuuka.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Hanae",
+    img: "7/72/Hanae.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Hare",
+    img: "4/47/Hare.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Hasumi",
+    img: "8/84/Hasumi.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Junko",
+    img: "3/3c/Junko.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Kayoko",
+    img: "a/a5/Kayoko.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Mutsuki",
+    img: "0/0b/Mutsuki.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Nonomi",
+    img: "a/ad/Nonomi.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Serika",
+    img: "c/c8/Serika.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Tsubaki",
+    img: "6/6a/Tsubaki.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Utaha",
+    img: "3/37/Utaha.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Yuuka",
+    img: "3/3e/Yuuka.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Asuna",
+    img: "9/9f/Asuna.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Chinatsu",
+    img: "b/b6/Chinatsu.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Haruka",
+    img: "b/b1/Haruka.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Juri",
+    img: "3/37/Juri.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Kotama",
+    img: "9/94/Kotama.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Kotori",
+    img: "a/ab/Kotori.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Pina",
+    img: "9/96/Pina.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Serina",
+    img: "4/41/Serina.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Shimiko",
+    img: "f/f4/Shimiko.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Suzumi",
+    img: "0/07/Suzumi.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Yoshimi",
+    img: "c/c2/Yoshimi.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Mashiro",
+    img: "0/06/Mashiro.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Izuna",
+    img: "5/54/Izuna.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Shizuko",
+    img: "7/77/Shizuko.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Alice",
+    img: "0/0f/Arisu.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Midori",
+    img: "e/ee/Midori.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Momoi",
+    img: "1/18/Momoi.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Cherino",
+    img: "7/7c/Cherino.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Nodoka",
+    img: "e/e1/Nodoka.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Yuzu",
+    img: "7/71/Yuzu.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Azusa",
+    img: "8/86/Azusa.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Hanako",
+    img: "9/9c/Hanako.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Koharu",
+    img: "c/c3/Koharu.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Azusa (Swimsuit)",
+    img: "a/a4/Azusa_%28Swimsuit%29.png",
+    opts: {
+		alt: true,
+		jp: false
+    }
+  },
+  {
+    name: "Mashiro (Swimsuit)",
+    img: "2/20/Mashiro_%28Swimsuit%29.png",
+    opts: {
+		alt: true,
+		jp: false
+    }
+  },
+  {
+    name: "Tsurugi (Swimsuit)",
+    img: "e/e6/Tsurugi_%28Swimsuit%29.png",
+    opts: {
+		alt: true,
+		jp: false
+    }
+  },
+  {
+    name: "Hifumi (Swimsuit)",
+    img: "3/32/Hifumi_%28Swimsuit%29.png",
+    opts: {
+		alt: true,
+		jp: false
+    }
+  },
+  {
+    name: "Hina (Swimsuit)",
+    img: "4/44/Hina_%28Swimsuit%29.png",
+    opts: {
+		alt: true,
+		jp: false
+    }
+  },
+  {
+    name: "Iori (Swimsuit)",
+    img: "5/53/Iori_%28Swimsuit%29.png",
+    opts: {
+		alt: true,
+		jp: false
+    }
+  },
+  {
+    name: "Izumi (Swimsuit)",
+    img: "b/b0/Izumi_%28Swimsuit%29.png",
+    opts: {
+		alt: true,
+		jp: false
+    }
+  },
+  {
+    name: "Shiroko (Cycling)",
+    img: "a/a0/Shiroko_%28Riding%29.png",
+    opts: {
+		alt: true,
+		jp: false
+    }
+  },
+  {
+    name: "Shun (Kid)",
+    img: "5/53/Shun_%28Kid%29.png",
+    opts: {
+		alt: true,
+		jp: false
+    }
+  },
+  {
+    name: "Kirino",
+    img: "8/85/Kirino.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Saya (Casual)",
+    img: "e/e2/Saya_%28Casual%29.png",
+    opts: {
+		alt: true,
+		jp: false
+    }
+  },
+  {
+    name: "Karin (Bunny Girl)",
+    img: "b/bd/Karin_%28Bunny_Girl%29.png",
+    opts: {
+		alt: true,
+		jp: false
+    }
+  },
+  {
+    name: "Neru (Bunny Girl)",
+    img: "7/78/Neru_%28Bunny_Girl%29.png",
+    opts: {
+		alt: true,
+		jp: false
+    }
+  },
+  {
+    name: "Asuna (Bunny Girl)",
+    img: "a/a4/Asuna_%28Bunny_Girl%29.png",
+    opts: {
+		alt: true,
+		jp: false
+    }
+  },
+  {
+    name: "Natsu",
+    img: "9/90/Natsu.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Mari",
+    img: "4/4f/Mari.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Hatsune Miku",
+    img: "9/93/Hatsune_Miku.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Ako",
+    img: "7/72/Ako.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Cherino (Hot Spring)",
+    img: "9/95/Cherino_%28Hot_Spring%29.png",
+    opts: {
+		alt: true,
+		jp: false
+    }
+  },
+  {
+    name: "Chinatsu (Hot Spring)",
+    img: "b/b1/Chinatsu_%28Hot_Spring%29.png",
+    opts: {
+		alt: true,
+		jp: false
+    }
+  },
+  {
+    name: "Tomoe",
+    img: "0/0a/Tomoe.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Nodoka (Hot Spring)",
+    img: "3/3f/Nodoka_%28Hot_Spring%29.png",
+    opts: {
+		alt: true,
+		jp: false
+    }
+  },
+  {
+    name: "Aru (New Year)",
+    img: "6/60/Aru_%28New_Year%29.png",
+    opts: {
+		alt: true,
+		jp: false
+    }
+  },
+  {
+    name: "Mutsuki (New Year)",
+    img: "c/c2/Mutsuki_%28New_Year%29.png",
+    opts: {
+		alt: true,
+		jp: false
+    }
+  },
+  {
+    name: "Serika (New Year)",
+    img: "6/61/Serika_%28New_Year%29.png",
+    opts: {
+		alt: true,
+		jp: false
+    }
+  },
+  {
+    name: "Wakamo",
+    img: "a/a7/Wakamo.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Fubuki",
+    img: "c/cd/Fubuki.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Chihiro",
+    img: "a/a7/Chihiro.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Sena",
+    img: "f/f4/Sena.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Mimori",
+    img: "8/82/Mimori.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Hinata",
+    img: "9/97/Hinata.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Ui",
+    img: "1/18/Ui.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Marina",
+    img: "9/9d/Marina.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Miyako",
+    img: "3/3e/Miyako.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Saki",
+    img: "d/d0/Saki.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Miyu",
+    img: "a/ac/Miyu.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Iroha",
+    img: "b/bf/Iroha.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Kaede",
+    img: "f/f0/Kaede.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Michiru",
+    img: "e/e6/Michiru.png",
+    opts: {
+		alt: false,
+		jp: false
+    }
+  },
+  {
+    name: "Tsukuyo",
+    img: "c/ce/Tsukuyo.png",
+    opts: {
+		alt: false,
+		jp: true
+    }
+  },
+  {
+    name: "Hiyori",
+    img: "7/76/Hiyori.png",
+    opts: {
+		alt: false,
+		jp: true
+    }
+  },
+  {
+    name: "Misaki",
+    img: "a/ae/Misaki.png",
+    opts: {
+		alt: false,
+		jp: true
+    }
+  },
+  {
+    name: "Atsuko",
+    img: "c/c7/Atsuko.png",
+    opts: {
+		alt: false,
+		jp: true
+    }
+  },
+  {
+    name: "Nonomi (Swimsuit)",
+    img: "9/98/Nonomi_%28Swimsuit%29.png",
+    opts: {
+		alt: true,
+		jp: true
+    }
+  },
+  {
+    name: "Wakamo (Swimsuit)",
+    img: "e/e2/Wakamo_%28Swimsuit%29.png",
+    opts: {
+		alt: true,
+		jp: true
+    }
+  },
+  {
+    name: "Ayane (Swimsuit)",
+    img: "f/f6/Ayane_%28Swimsuit%29.png",
+    opts: {
+		alt: true,
+		jp: true
+    }
+  },
+  {
+    name: "Hoshino (Swimsuit)",
+    img: "c/c2/Hoshino_%28Swimsuit%29.png",
+    opts: {
+		alt: true,
+		jp: true
+    }
+  },
+  {
+    name: "Shizuko (Swimsuit)",
+    img: "f/f2/Shizuko_%28Swimsuit%29.png",
+    opts: {
+		alt: true,
+		jp: true
+    }
+  },
+  {
+    name: "Chise (Swimsuit)",
+    img: "1/1e/Chise_%28Swimsuit%29.png",
+    opts: {
+		alt: true,
+		jp: true
+    }
+  },
+  {
+    name: "Izuna (Swimsuit)",
+    img: "3/37/Izuna_%28Swimsuit%29.png",
+    opts: {
+		alt: true,
+		jp: true
+    }
+  },
+  {
+    name: "Saori",
+    img: "4/46/Saori.png",
+    opts: {
+		alt: false,
+		jp: true
+    }
+  },
+  {
+    name: "Kazusa",
+    img: "b/b3/Kazusa.png",
+    opts: {
+		alt: false,
+		jp: true
+    }
+  },
+  {
+    name: "Moe",
+    img: "c/cc/Moe.png",
+    opts: {
+		alt: false,
+		jp: true
+    }
+  },
+  {
+    name: "Kokona",
+    img: "8/83/Kokona.png",
+    opts: {
+		alt: false,
+		jp: true
+    }
+  },
+  {
+    name: "Noa",
+    img: "2/23/Noa.png",
+    opts: {
+		alt: false,
+		jp: true
+    }
+  },
+  {
+    name: "Utaha (Cheerleader)",
+    img: "7/7b/Utaha_%28Cheerleader%29.png",
+    opts: {
+		alt: true,
+		jp: true
+    }
+  },
+  {
+    name: "Hibiki (Cheerleader)",
+    img: "b/bf/Hibiki_%28Cheerleader%29.png",
+    opts: {
+		alt: true,
+		jp: true
+    }
+  },
+  {
+    name: "Akane (Bunny Girl)",
+    img: "9/99/Akane_%28Bunny_Girl%29.png",
+    opts: {
+		alt: true,
+		jp: true
+    }
+  },
+  {
+    name: "Mari (Sportswear)",
+    img: "8/88/Mari_%28Sportswear%29.png",
+    opts: {
+		alt: true,
+		jp: true
+    }
+  },
+  {
+    name: "Yuuka (Sportswear)",
+    img: "6/60/Yuuka_%28Sportswear%29.png",
+    opts: {
+		alt: true,
+		jp: true
+    }
+  },
+  {
+    name: "Hasumi (Sportswear)",
+    img: "8/80/Hasumi_%28Sportswear%29.png",
+    opts: {
+		alt: true,
+		jp: true
+    }
+  },
+  {
+    name: "Himari",
+    img: "5/5a/Himari.png",
+    opts: {
+		alt: false,
+		jp: true
+    }
+  },
+  {
+    name: "Shigure",
+    img: "d/da/Shigure.png",
+    opts: {
+		alt: false,
+		jp: true
+    }
+  },
+  {
+    name: "Arona",
+    img: "thumb/e/e6/Arona.png/48px-Arona.png",
+    opts: {
+		alt: false,
+		jp: false,
+		npc: true
+    }
+  },
+  {
+    name: "Momoka",
+    img: "thumb/7/73/Momoka.png/48px-Momoka.png",
+    opts: {
+		alt: false,
+		jp: false,
+		npc: true
+    }
+  },
+  {
+    name: "Sora",
+    img: "thumb/9/99/Sora.png/48px-Sora.png",
+    opts: {
+		alt: false,
+		jp: false,
+		npc: true
+    }
+  },
+  {
+    name: "Rin",
+    img: "thumb/8/87/Rin.png/48px-Rin.png",
+    opts: {
+		alt: false,
+		jp: false,
+		npc: true
+    }
+  },
+];
